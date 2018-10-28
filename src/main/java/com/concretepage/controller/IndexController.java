424f2b77947ce08bb6c0c2395a4b96f8811c94b0
phongnguyen87 <phongnguyenitb@gmail.com>
2018-10-29 04:26:19 +0700

    add project to github

---
+++ src/main/java/com/concretepage/controller/IndexController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.concretepage.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ *
+ * @author Bill
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("*")
+    public String index(HttpServletRequest request) {
+        System.out.println(request.getServletPath());
+        return "index";
+    }
+}
